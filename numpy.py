5b1444e37fbcd5eb7bc7ed2fc6f714ab91f16620
Ishant0301 <160219035+Ishant0301@users.noreply.github.com>
2024-04-12 16:44:30 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,21 @@
+import numpy as np
+# a = np.array([[12,6,9,6],[2,9,9,5],[3,5,8,5],[3,4,6,8]])
+# print(a)
+# print(a.ndim)                       TELLS DIMENSION OF ARRAY
+# print(a.itemsize)
+# print(a.size)                         ELEMENTS NO. OF ARRAY
+# print(a.shape)
+# a = np.zeros((3,4),dtype = complex  )
+# print(a)
+# array = np.arange(1,10,2)
+# print(array)
+# a = np.linspace(1,6,13)
+# print(a)
+# print(a.reshape(8,2))             WE CAN NORMALLY CHANGE THE N AND M OF MATRIX
+# print(a.ravel())                  BASICALLY BRING ALL ELEMENT IN [1 X M] FORMAT WHERE M IS NO. OF ELEMENTS
+# print(a.sum(axis = 1))            
+# b = np.sqrt(a)
+# print(b)
+# b = np.array([[12,6,8,9],[2,9,3,6],[3,5,4,6],[3,4,7,0]])
+# print(a*b)                   # BOTH ARRAY SHOULD HAVE SAME N X M 
+# print(a.dot(b))
